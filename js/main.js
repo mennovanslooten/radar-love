2a22ec4e2d63238a95e9e7c16f5372f3882141d3
Menno van Slooten <menno@backbase.com>
2017-09-09 09:28:04 +0200

    Simplified radar target code

--- js/main.js
+++ js/main.js
@@ -1,151 +1,141 @@
 (function () {
     'use strict';
 
-    const tau = Math.PI * 2;
-
     const docHeight = document.body.clientHeight;
     const docWidth = document.body.clientWidth;
     const centerX = docWidth / 2;
     const centerY = docHeight / 2;
 
+    const app = (function() {
+        const fps = 30;
+        const state = {
+            isTouched: false,
+            touchX: 0,
+            touchY: 0,
+            frame: 0,
+            fps
+        };
+        const listeners = [];
+
+        function scheduleTick() {
+            setTimeout(tick, 1000 / fps);
+        }
+
+
+        function tick() {
+            state.frame++;
+            listeners.forEach(cb => {
+                cb(state);
+            });
+            scheduleTick();
+        }
+
+
+        function setPosition(e) {
+            e.preventDefault();
+            state.touchX = e.touches[0].clientX;
+            state.touchY = e.touches[0].clientY;
+        }
+
+
+        function onTouchStart(e) {
+            e.preventDefault();
+            state.isTouched = true;
+        }
+
+
+        function onTouchEnd(e) {
+            e.preventDefault();
+            state.isTouched = false;
+        }
+
+
+        function ontick(cb) {
+            listeners.push(cb);
+        }
+
+        document.addEventListener("touchstart", setPosition, { passive: false });
+        document.addEventListener("touchstart", onTouchStart, { passive: false });
+        document.addEventListener("touchend", onTouchEnd, { passive: false });
+        document.addEventListener("touchmove", setPosition, { passive: false });
+
+        scheduleTick();
+
+        return { ontick };
+    })();
 
     function main() {
-        const target = initTarget();
-        initBeam(target);
+        initBeam();
     }
+
 
 
     /* ------------------------
     RADAR BEAM
     ------------------------ */
-    function initBeam(target) {
+    function initBeam() {
         const elt = document.querySelector('.beam');
-        const startTime = new Date().valueOf();
+        const loopTime = 2;
 
-        function getLoopPerc() {
-            const currTime = new Date().valueOf();
-            const deltaTime = currTime - startTime;
-            const loopTime = 2000;
-            return (deltaTime % loopTime) / loopTime * 100;
+        function getBeamAngle(appState) {
+            const tau = Math.PI * 2;
+            const framesPerLoop = loopTime * 1000 / appState.fps;
+            const currFrame = appState.frame % framesPerLoop;
+            return (tau * currFrame / framesPerLoop) - Math.PI;
         }
 
 
-        function rotateBeam() {
-            const perc = getLoopPerc();
-            const beamAngle = (tau / 100 * perc) - Math.PI;
+        function update(appState) {
+            const beamAngle = getBeamAngle(appState);
             elt.style.transform = (`rotate(${beamAngle}rad)`);
-            target.notify(beamAngle);
-            loop();
+
+            if (checkAngle(beamAngle, appState)) {
+                createTarget(appState);
+            }
         }
 
+        app.ontick(update)
+    }
 
-        function loop() {
-            requestAnimationFrame(rotateBeam);
-        }
 
-        loop()
+    function getTargetCoordinates(appState) {
+        const offsetX = appState.touchX - centerX;
+        const offsetY = appState.touchY - (centerY * 1.5);
+
+        const x = Math.max(0, offsetX * 2);
+        const y = Math.max(0, offsetY * 2);
+
+        return { x, y };
     }
 
 
     /* ------------------------
     RADAR TARGET
     ------------------------ */
-    function initTarget() {
-        const elt = document.querySelector('.target');
-        let isVisible = false;
-        const coordinates = {
-            x: 0,
-            y: 0
-        };
-
-
-        function moveTarget(e) {
-            e.preventDefault();
-            const touchX = e.touches[0].clientX;
-            const touchY = e.touches[0].clientY;
-            repositionTarget(touchX, touchY);
+    function checkAngle(beamAngle, appState) {
+        if (!appState.isTouched) {
+            return false;
         }
 
+        const { x, y } = getTargetCoordinates(appState);
+        const targetAngle = Math.atan((x - centerX) / (centerY - y));
+        const deltaAngle = Math.abs(targetAngle - beamAngle);
 
-        function getPosition(touchX, touchY) {
-            const offsetX = touchX - centerX;
-            const offsetY = touchY - (centerY * 1.5);
-            if (offsetX < 0 || offsetY < 0) {
-                return { x: 0, y: 0 };
-            }
-            const x = offsetX * 2;
-            const y = offsetY * 2;
-            return { x, y };
-        }
-
-
-        function setCoordinates(x, y) {
-            coordinates.x = x - centerX;
-            coordinates.y = centerY - y;
-        }
-
-
-        function getAngle() {
-            if (!isVisible) {
-                return;
-            }
-
-            const { x, y } = coordinates;
-            const targetAngle = Math.atan(x / y);
-            return targetAngle;
-        }
-
-
-        function notify(beamAngle) {
-            const targetAngle = getAngle();
-            if (targetAngle) {
-                const deltaAngle = Math.abs(targetAngle - beamAngle);
-                toggleTarget(deltaAngle < 0.2);
-            }
-        }
-
-
-        function repositionTarget(touchX, touchY) {
-            const { x, y } = getPosition(touchX, touchY);
-            elt.style.left = x + 'px';
-            elt.style.top = y + 'px';
-            setCoordinates(x, y);
-        }
-
-
-        function getDistanceFromCenter() {
-            const { x, y } = coordinates;
-            return Math.sqrt(x * x + y * y);
-        }
-
-
-        function toggleTarget(isActive) {
-            const distance = getDistanceFromCenter();
-            elt.classList.toggle('target--active', isActive);
-            elt.classList.toggle('target--close', distance < 50);
-        }
-
-
-        function showTarget(e) {
-            e.preventDefault();
-            isVisible = true;
-            elt.classList.add('target--visible');
-        }
-
-
-        function hideTarget() {
-            isVisible = false;
-            elt.classList.remove('target--visible');
-        }
-
-        document.addEventListener("touchstart", showTarget, { passive: false });
-        document.addEventListener("touchend", hideTarget, { passive: false });
-        document.addEventListener("touchmove", moveTarget, { passive: false });
-
-        return {
-            notify: notify
-        }
+        return deltaAngle < 0.05;
     }
+
+
+    function createTarget(appState) {
+        const { x, y } = getTargetCoordinates(appState);
+        const elt = document.createElement('div');
+        elt.className = 'target';
+
+        elt.style.left = x + 'px';
+        elt.style.top = y + 'px';
+
+        document.body.appendChild(elt);
+    }
+
 
     // Deal with resize events, the easy way
     window.addEventListener("resize", location.reload.bind(location));
